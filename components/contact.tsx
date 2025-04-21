d5edc11faafbb32f8cf477825e934bf764d555d3
M1D0R1x <veerababusaviti21@gmail.com>
2025-04-22 03:41:15 +0530

    fix: handle unused variable warning in contact form error handling

--- components/contact.tsx
+++ components/contact.tsx
@@ -61,6 +61,7 @@ export default function Contact() {
             } else {
                 throw new Error("Failed to send message")
             }
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
         } catch (error) {
             setFormStatus({
                 type: "error",
